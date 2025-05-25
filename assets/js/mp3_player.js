da5b4a6e21e57a7eb385d43a8892d7bd9c29a521
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-25 14:28:23 +0800

    fix: Implement structural updates and optimizations across multiple modules

---
+++ assets/js/mp3_player.js
@@ -0,0 +1,9 @@
+const BASE_URL = window.BASE_URL || '/';
+
+const songs = [
+    {
+        title: "Seishun Complex",
+        file: BASE_URL + "assets/mp3/seishun complex.mp3",
+        logo: BASE_URL + "assets/img/seishun complex.jpg"
+    }
+]
