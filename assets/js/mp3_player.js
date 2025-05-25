60e10f0953869af14c7929b625def975840bd982
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-25 14:36:24 +0800

    chore: moved song images to separate folder to reduce clutter and improve organization

--- assets/js/mp3_player.js
+++ assets/js/mp3_player.js
@@ -2,8 +2,13 @@ const BASE_URL = window.BASE_URL || '/';
 
 const songs = [
     {
-        title: "Seishun Complex",
+        title: "seishun complex",
         file: BASE_URL + "assets/mp3/seishun complex.mp3",
-        logo: BASE_URL + "assets/img/seishun complex.jpg"
+        logo: BASE_URL + "assets/img/Songs/seishun complex.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/Songs/hitoribocchi tokyo.jpg"
     }
 ]
