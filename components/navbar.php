f32fb308632ce20950c4a19550b55cb8dceb62ad
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-25 09:38:59 +0800

    feat: add navbar component for site navigation

---
+++ components/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="<?php echo BASE_URL; ?>index.php">Home</a></li>
+        <li><a href="<?php echo BASE_URL; ?>about.php">About</a></li>
+        <li><a href="<?php echo BASE_URL; ?>characters.php">Characters</a></li>
+        <li><a href="<?php echo BASE_URL; ?>band_info.php">Band Info</a></li>
+        <li><a href="<?php echo BASE_URL; ?>contact.php">Contact</a></li>
+    </ul>
+</nav>
