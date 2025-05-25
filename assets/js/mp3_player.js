93adb047dcd2a94c0ddfecc203f2a0c1c7b5eed8
Mikaelaa05 <bumble.macz@yahoo.com>
2025-05-25 14:50:20 +0800

    feat: added The Little Sea song and image

--- assets/js/mp3_player.js
+++ assets/js/mp3_player.js
@@ -10,5 +10,135 @@ const songs = [
         title: "hitoribocchi tokyo",
         file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
         logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
-    }
+    },
+    {
+        title: "Distortion!!",
+        file: BASE_URL + "assets/mp3/Distortion!!.mp3",
+        logo: BASE_URL + "assets/img/songs/Distortion!!jpg"
+    },
+    {
+        title: "Secret base",
+        file: BASE_URL + "assets/mp3/Secret base.mp3",
+        logo: BASE_URL + "assets/img/songs/Secret base.jpg"
+    },
+    {
+        title: "Guitar, Loneliness and Blue Planet",
+        file: BASE_URL + "assets/mp3/Guitar, Loneliness and Blue Planet.mp3",
+        logo: BASE_URL + "assets/img/songs/Guitar, Loneliness and Blue Planet.jpg"
+    },
+    {
+        title: "I can't sing a love song",
+        file: BASE_URL + "assets/mp3/I can't sing a love song.mp3",
+        logo: BASE_URL + "assets/img/songs/I can't sing a love song.jpg"
+    },
+    {
+        title: "That band",
+        file: BASE_URL + "assets/mp3/That band.mp3",
+        logo: BASE_URL + "assets/img/songs/That band.jpg"
+    },
+    {
+        title: "Karakara",
+        file: BASE_URL + "assets/mp3/Karakara.mp3",
+        logo: BASE_URL + "assets/img/songs/Karakara.jpg"
+    },
+    {
+        title: "The Little Sea",
+        file: BASE_URL + "assets/mp3/The Little Sea.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
+    {
+        title: "hitoribocchi tokyo",
+        file: BASE_URL + "assets/mp3/hitoribocchi tokyo.mp3",
+        logo: BASE_URL + "assets/img/songs/hitoribocchi tokyo.jpg"
+    },
 ]
