11acf7a3642188a959d7b508071d6a371987a46a
Mikaelaa05 <bumble.macz@yahoo.com>
2025-07-08 13:37:14 +0800

    feat: create main layout file to structure page with navbar and footer

---
+++ layouts/main.layout.php
@@ -0,0 +1,9 @@
+<?php
+include BASE_PATH . '/components/navbar.component.php';
+?>
+<main>
+    <?php include $content; ?>
+</main>
+<?php
+include BASE_PATH . '/components/footer.component.php';
+?>
