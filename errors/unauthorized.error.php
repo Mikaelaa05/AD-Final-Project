e0e1b124cc5578e1d831b7278144ac921e0d874e
Mikaelaa05 <bumble.macz@yahoo.com>
2025-07-08 13:36:56 +0800

    feat: add 401 Unauthorized error page

---
+++ errors/unauthorized.error.php
@@ -0,0 +1,17 @@
+<?php
+http_response_code(401);
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>401 Unauthorized</title>
+    </head>
+
+    <body>
+        <h1>401 Unauthorized</h1>
+        <p>You must be logged in to view this page.</p>
+        <a href="/pages/Login/index.php">Go to Login</a>
+    </body>
+
+</html>
