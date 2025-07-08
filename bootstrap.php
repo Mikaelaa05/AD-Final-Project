4b25fc78911cd1dfa3e9a919e56b229244c1fafc
Mikaelaa05 <bumble.macz@yahoo.com>
2025-07-08 13:36:16 +0800

    feat: define UTILS_PATH and DUMMIES_PATH constants in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,6 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
+define('DUMMIES_PATH', realpath(BASE_PATH . '/staticData/dummies'));
 
 chdir(BASE_PATH);
