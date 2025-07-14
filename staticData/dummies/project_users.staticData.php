8b5ff26da1e3146e9b5e9a3ee3bfa6a2c0d6caf5
Mikaelaa05 <bumble.macz@yahoo.com>
2025-07-14 23:43:45 +0800

    fix: add dummy data for project users in staticData

---
+++ staticData/dummies/project_users.staticData.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    [
+        'project_id' => null, // Fill with actual UUID after seeding projects
+        'user_id' => null     // Fill with actual UUID after seeding users
+    ]
+];
