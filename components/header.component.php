e35414902daadabdf999637c265f20066ba3a845
DrowningAnother <202312208@fit.edu.ph>
2025-07-16 23:25:06 +0800

    feat: added header component

---
+++ components/header.component.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Header Component
+ * Contains the navbar and any other header content
+ */
+?>
+<header>
+    <?php include BASE_PATH . '/components/navbar.component.php'; ?>
+</header>
