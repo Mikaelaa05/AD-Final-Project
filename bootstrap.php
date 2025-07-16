0578c3bbbeb6351031a23d5ab42af87f628facff
Nobody-Afk <micjolaguesma@gmail.com>
2025-07-16 20:26:26 +0800

    feat: Added bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,4 @@
 define('BASE_PATH', realpath(__DIR__));
 define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
 define('DUMMIES_PATH', realpath(BASE_PATH . '/staticData/dummies'));
-
 chdir(BASE_PATH);
