2f186545e709fe3f13766de75d391bd150b2bcf0
BorisDelaCruz <202311499@fit.edu.ph>
2025-07-16 18:42:56 +0800

    feat: add additional user entries to static data for enhanced testing scenarios

--- staticData/dummies/users.staticData.php
+++ staticData/dummies/users.staticData.php
@@ -9,4 +9,40 @@ return [
         'phone' => '09123456789',
         'role' => 'Database Manager'
     ],
+    [
+        'username' => 'MikaTheRock',
+        'first_name' => 'Mikaela Andrea',
+        'last_name' => 'Cid',
+        'password' => 'admin',
+        'email' => 'mikaela.cid@gmail.com',
+        'phone' => '09876543210',
+        'role' => 'Quality Assurance Manager'
+    ],
+    [
+        'username' => 'SusPeekZ',
+        'first_name' => 'Jan-Michael II',
+        'last_name' => 'Laguesma',
+        'password' => 'admin',
+        'email' => 'micjolaguesma@gmail.com',
+        'phone' => '09234567891',
+        'role' => 'Backend'
+    ],
+    [
+        'username' => 'Jam',
+        'first_name' => 'Baron Jamille',
+        'last_name' => 'Andres',
+        'password' => 'admin',
+        'email' => 'mung.andres@gmail.com',
+        'phone' => '09345678912',
+        'role' => 'Designer'
+    ],
+    [
+        'username' => 'Waffle',
+        'first_name' => 'Syrrlian',
+        'last_name' => 'Castro',
+        'password' => 'admin',
+        'email' => 'punzubzero@email.com',
+        'phone' => '09456789123',
+        'role' => 'Front-End Developer'
+    ]
 ];
