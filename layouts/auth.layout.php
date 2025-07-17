0dc5d9b3c98a8b21d18834cf59f66b7e12b530e6
DrowningAnother <202312208@fit.edu.ph>
2025-07-17 10:52:39 +0800

    feat: added authenticator css to see the general layout for the auth

---
+++ layouts/auth.layout.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Authentication Layout
+ * Used for login and signup pages
+ * Does not include header/navbar since users are not authenticated
+ */
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title><?php echo isset($pageTitle) ? htmlspecialchars($pageTitle) : 'Authentication'; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/auth.css">
+</head>
+
+<body>
+    <div class="auth-container">
+        <?php echo $content; ?>
+    </div>
+    <?php include BASE_PATH . '/components/footer.component.php'; ?>
+</body>
+
+</html>
