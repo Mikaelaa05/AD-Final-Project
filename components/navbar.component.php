58bd22df87031b39aadfbbe849e0b791dbe2887b
Nobody-Afk <micjolaguesma@gmail.com>
2025-07-21 22:03:26 +0800

    Refactor: updated navbar components

--- components/navbar.component.php
+++ components/navbar.component.php
@@ -1,7 +1,15 @@
 <nav>
     <a href="/">Home</a>
     <a href="/pages/Shop">Shop</a>
-    <a href="/pages/Cart">Cart</a>
+    <a href="/pages/Cart" class="cart-link">
+        Cart 
+        <?php 
+        $cartCount = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;
+        if ($cartCount > 0): 
+        ?>
+            <span class="cart-count"><?= $cartCount ?></span>
+        <?php endif; ?>
+    </a>
     <a href="/pages/About">About</a>
     <?php if (isset($_SESSION['user']) && function_exists('isAdmin') && isAdmin()): ?>
         <a href="/pages/Admin">Admin</a>
